bc86a1ffd257192e1f8b433155d2d22b2532643c
xyz <stevengetsby@163.com>
2025-04-20 23:37:14 +0800

    bayes_regression

---
+++ supervised_learning/Bayes_Regression.py
@@ -0,0 +1,10 @@
+
+class Bayes_Regression():
+    """
+    n_draws:抽取模拟的次数
+    poly_degree:用于多项式回归的次数
+    cred_int:置信区间
+    """
+    def __init__(self, n_draws, mu0, omega0, nu0, sigma_sq0, poly_degree = 0, cred = 95):
+        return
+        
